718d0dfc31ffc2937b4ec073f636596c20e2317b
supAutomatizacion <supervisor.automatizacion@saceites.com>
2026-01-08 11:06:39 -0500

    segundo commit - autopass in progress

---
+++ components/headerComponent.tsx
@@ -0,0 +1,7 @@
+export default function HeaderComponent () {
+    return (
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b">
+            LOGO SACEITES | FECHA ACTUAL/MTTO | TOTAL ACTIVIDADES | ULT. ACTUALIZACION
+        </header>
+    )
+}
