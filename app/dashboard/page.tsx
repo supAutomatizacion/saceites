44fcb482a0c2bf73cbe8d8908211f47a5d874592
HaderCabrera <hader.cabrera96@gmail.com>
2026-02-10 15:08:08 -0500

    organizacion 02102026

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,11 @@
 
 export default function Page() {
   return (
-    <div style={{ height: "100vh", background: "#111", color: "white", padding: 20 }}>
-      <h1>DASHBOARD QUE VIENE DE POWER BI</h1>
-
+    <div className="h-[calc(100vh-(var(--header-height))-16px)] bg-saceites-2 p-2 rounded-b-xl">
       <iframe
         title="test_dashboard"
         src="https://app.powerbi.com/reportEmbed?reportId=edc2bcda-db6d-4db7-a99a-061cef07603c&autoAuth=true&ctid=cbcbf211-eab4-4808-ad3f-e9b448bd3320&actionBarEnabled=true"
-        style={{ width: "100%", height: "90%", border: "none" }}
+        style={{ width: "100%", height: "100%", border: "none" }}
         allowFullScreen
       />
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 
+import { redirect } from 'next/navigation'
+
 export default function Home() {
 
-  return null;
+  redirect('/dashboard')
+  
 }
